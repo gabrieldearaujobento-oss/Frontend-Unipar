e9e220263c62854218fabc87d0c2818e798283c5
gabrieldearaujobento-oss <gabrieldearaujobento@gmail.com>
2026-09-06 23:51:06 -0300

    fix: enviando ativ1

---
+++ Desenvolvimento Web/PrimeiroBimestre/Aulas/aula5/exemplovars.js
@@ -0,0 +1,47 @@
+let nome;
+
+const nomeNulo = null;
+
+const aluno = {
+    id: 123213,
+    nome: "Gabriel",
+    ativo: true,
+    pos: null,
+    dataNascimento : new Date("26/11/1998"),
+    endereco: {
+        rua: "Elias Abraao",
+        numero: 2321,
+        bairro: "Paulo Godoy",
+        cidade: "Cascavel"
+    }
+}
+
+const frutas = ["🍊","🍉","🍇","🍌"];
+
+console.log(frutas)
+
+function soma(n1, n2, printarNome) {
+    return n1 + n2;
+}
+
+console.log(soma(20, 40));
+console.log(() => 2 + 6)
+
+
+const valor1 = "2";
+const valor2 = 2;
+
+// dois igaul = = checa apenas valor
+if (valor1 === valor2) {
+    console.log("É igual!!")
+}
+
+//três igual = = = checa tipo e valor
+if (valor1 === valor2) {
+    console.log("É igual!!")
+}
+
+for (let console.log = 0; console.log < array.length; console.log++) {
+    const element = array[console.log];
+    console.log(element)
+}
